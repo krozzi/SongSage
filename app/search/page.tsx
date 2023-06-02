399bb40eb60c72030bdab785af41bf8ae8b5cb70
bigNasY <bobby.j.smith@icloud.com>
2023-06-02 01:50:43 -0700

    wip of thing
    csxccxv

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import {useEffect, useState, ChangeEvent} from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Dashboard from "../dashboard/page";
 import SongResult from "@/components/SongResult";
@@ -12,174 +12,172 @@ const CLIENT_SECRET = process.env.NEXT_PUBLIC_SPOTIPAL_CLIENT_SECRET;
 const REDIRECT_URI = "http://localhost:3000/dashboard";
 
 export default function Search() {
-      const [searchText, setSearchText] = useState("");
-      const [searchResults, setSearchResults] = useState<string[]>([]);
-      const [access_token, setAccessToken] = useState("");
-      const router = useRouter();
-      
-      const searchParams = useSearchParams();
-      const code = searchParams.get("code");
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [access_token, setAccessToken] = useState("");
+  const router = useRouter();
 
-      function authorizeSpotify(redirect:string) {
-        const scopes = ["user-read-private", "user-read-email", "user-top-read", "playlist-read-private"];
-    
-        const queryParams = {
-          client_id: CLIENT_ID,
-          response_type: "code",
-          redirect_uri: redirect,
-          scope: scopes.join(" "),
-        };
-    
-        const authorizationUrl = `https://accounts.spotify.com/authorize?${querystring.stringify(
-          queryParams
-        )}`;
-    
-        router.push(authorizationUrl);
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+
+  function authorizeSpotify(redirect: string) {
+    const scopes = [
+      "user-read-private",
+      "user-read-email",
+      "user-top-read",
+      "playlist-read-private",
+    ];
+
+    const queryParams = {
+      client_id: CLIENT_ID,
+      response_type: "code",
+      redirect_uri: redirect,
+      scope: scopes.join(" "),
+    };
+
+    const authorizationUrl = `https://accounts.spotify.com/authorize?${querystring.stringify(
+      queryParams
+    )}`;
+
+    router.push(authorizationUrl);
+  }
+
+  interface TrackResult {
+    name: string;
+    artists: string[];
+    coverUrl: string;
+  }
+
+  const performSearch = async (searchText: string) => {
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+          searchText
+        )}&type=track`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Search request failed");
       }
 
-      interface TrackResult {
-        name: string;
-        artists: string[];
-        coverUrl: string;
-      }
-      
-      const performSearch = async (searchText: string) => {
+      const data = await response.json();
+      const searchResults = data.tracks.items.map((item: any) => ({
+        id: item.id,
+        name: item.name,
+        artists: item.artists.map((artist: any) => artist.name).join(", "),
+        coverUrl: item.album.images[0].url,
+      }));
+      setSearchResults(searchResults);
+    } catch (error) {
+      console.error("Error performing search:", error);
+      setSearchResults([]);
+    }
+  };
+
+  useEffect(() => {
+    async function getAccessToken() {
+      if (CLIENT_SECRET) {
         try {
           const response = await fetch(
-            `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchText)}&type=track`,
+            "https://accounts.spotify.com/api/token",
             {
+              method: "POST",
               headers: {
-                Authorization: `Bearer ${access_token}`,
-                "Content-Type": "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
               },
+              body: new URLSearchParams({
+                grant_type: "authorization_code",
+                code: code?.toString() ?? "",
+                redirect_uri: "http://localhost:3000/search",
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+              }).toString(),
             }
           );
-    
-          if (!response.ok) {
-            throw new Error("Search request failed");
+
+          if (response.status !== 200) {
+            console.log("User token expired... reauthing...");
+            authorizeSpotify("http://localhost:3000/search");
           }
-    
+
           const data = await response.json();
-          const searchResults = data.tracks.items.map((item: any) => ({
-            id: item.id,
-            name: item.name,
-            artists: item.artists.map((artist: any) => artist.name).join(", "),
-            coverUrl: item.album.images[0].url,
-          }));
-          setSearchResults(searchResults);
+          const { access_token } = data;
+          setAccessToken(access_token);
+
+          localStorage.setItem("accessToken", access_token);
+
+          return access_token;
         } catch (error) {
-          console.error("Error performing search:", error);
-          setSearchResults([]);
-        }
-      };
-    
-
-      useEffect(() => {
-        
-        async function getAccessToken() {
-          if (CLIENT_SECRET) {
-            try {
-              const response = await fetch(
-                "https://accounts.spotify.com/api/token",
-                {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                  },
-                  body: new URLSearchParams({
-                    grant_type: "authorization_code",
-                    code: code?.toString() ?? "",
-                    redirect_uri: "http://localhost:3000/search",
-                    client_id: CLIENT_ID,
-                    client_secret: CLIENT_SECRET,
-                  }).toString(),
-                }
-              );
-
-              if(response.status !== 200) {
-                console.log("User token expired... reauthing...");
-                authorizeSpotify("http://localhost:3000/search");
-              }
-    
-              const data = await response.json();
-              const { access_token } = data;
-              setAccessToken(access_token);
-
-              localStorage.setItem("accessToken", access_token);
-              
-              return access_token;
-            } catch (error) {
-              console.error(error);
-              console.log("User token expired... reauthing...");
-              authorizeSpotify("http://localhost:3000/search");
-            }
-          }
-        }
-
-
-        if(code) {
-          getAccessToken();
-        } else {
-          console.log("User not signed in, redirecting to authorization page...");
+          console.error(error);
+          console.log("User token expired... reauthing...");
           authorizeSpotify("http://localhost:3000/search");
         }
-          
-      }, []);
+      }
+    }
 
-      const handleSearch = () => {
-        console.log(searchText);
-      };
-      const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setSearchText(event.target.value);
-        console.log(performSearch(event.target.value));
-        
-      };
+    if (code) {
+      getAccessToken();
+    } else {
+      console.log("User not signed in, redirecting to authorization page...");
+      authorizeSpotify("http://localhost:3000/search");
+    }
+  }, []);
 
-      return (
-        <div className="hero h-screen no-scrollbar">
-              
-            
-          <div className="mb-[60rem]">
+  const handleSearch = () => {
+    console.log(searchText);
+  };
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+    console.log(performSearch(event.target.value));
+  };
 
-              <h1 className="pt-32 pb-16 text-center font-poppins text-5xl md:text-6xl lg:text-7xl font-bold">
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-accent to-cyan-400">Discover </span>
-                new songs
-              </h1>
+  return (
+    <div className="hero h-screen no-scrollbar">
+      <div className="mb-[60rem]">
+        <h1 className="pt-32 pb-16 text-center font-poppins text-5xl md:text-6xl lg:text-7xl font-bold">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-accent to-cyan-400">
+            Discover{" "}
+          </span>
+          new songs
+        </h1>
 
-
-              <div className="input-group flex justify-center">
-                  <input
-                      type="text"
-                      placeholder="Find similar songs"
-                      className="input join-item input-lg input-bordered lg:w-[50rem] md:w-[30rem] font-poppins"
-                      value={searchText}
-                      onChange={handleChange}
-                    />
-                  <button onClick={handleSearch} className="drop-shadow-[0_4px_145px_rgba(29,185,84,255)] btn join-item btn-lg btn-accent font-poppins text-white normal-case text-xl">
-                    Search
-                  </button> 
-              </div>
-              
-          </div>
-
-
-          <div style={{ maxHeight: "500px"}}>
-            <ul>
-              {searchResults.map((result, index) => (
-                <li key={index} className="py-2">
-
-                  <SongResult 
-                    title={result.name}
-                    artists={result.artists}
-                    image={result.coverUrl}
-                    id={result.id}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-
+        <div className="input-group flex justify-center">
+          <input
+            type="text"
+            placeholder="Find similar songs"
+            className="input join-item input-lg input-bordered lg:w-[50rem] md:w-[30rem] font-poppins"
+            value={searchText}
+            onChange={handleChange}
+          />
+          <button
+            onClick={handleSearch}
+            className="drop-shadow-[0_4px_145px_rgba(29,185,84,255)] btn join-item btn-lg btn-accent font-poppins text-white normal-case text-xl"
+          >
+            Search
+          </button>
+        </div>
       </div>
-  
-    );
+
+      <div style={{ maxHeight: "500px" }}>
+        <ul>
+          {searchResults.map((result, index) => (
+            <li key={index} className="py-2">
+              <SongResult
+                title={result.name}
+                artists={result.artists}
+                image={result.coverUrl}
+                id={result.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- app/song/page.tsx
+++ app/song/page.tsx
@@ -1,7 +1,107 @@
-export default function SongPage() {
-    return (
-        <div className="hero h-screen">
-          <h1 className="font-poppins font-semibold px-10 lg:px-0 text-3xl lg:text-4xl text-center">Hi blud this page is currently under construction</h1>
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface Song {
+  name: string;
+  artists: any[];
+  image: string;
+}
+
+async function getSong(query: string, token: string) {
+  const response = await fetch(
+    `https://api.spotify.com/v1/tracks/${encodeURIComponent(query)}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Search request failed");
+  }
+
+  return response.json();
+}
+
+async function getRecs(query: string, token: string) {
+  const res = await fetch(
+    `https://api.spotify.com/v1/recommendations?limit=15&seed_tracks=${query}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("woopsie");
+  }
+
+  return res.json();
+}
+
+export default async function SongPage() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("song_id");
+  const token = localStorage.getItem("accessToken");
+  console.log(id);
+  console.log(token);
+
+  var displaySong: Song = {
+    name: "",
+    artists: [],
+    image: "",
+  };
+
+  var recommendeds: Song[] = [];
+
+  if (id && token) {
+    const sog = await getSong(id, token);
+    displaySong = {
+      name: sog.name,
+      artists: sog.artists.map((art: any) => art.name),
+      image: sog.album.images[0].url,
+    };
+    console.log(displaySong);
+
+    const recs = await getRecs(id, token);
+    console.log(recs);
+    recs.tracks.map((track: any) => {
+      const thing: Song = {
+        name: track.name,
+        artists: track.artists,
+        image: track.album.images[0].url,
+      };
+      recommendeds.push(thing);
+    });
+    console.log(recommendeds);
+  }
+
+  return (
+    <>
+      <div className="flex lg:flex-row md:flex-row flex-col pt-11 pl-12 pb-7 border-b-4 border-accent">
+        <div>
+          <img
+            src={displaySong.image}
+            className=" lg:w-96 lg:h-96 pl-20 pt-20 md:w-80 md:h-80 w-64 h-64 rounded-lg"
+          ></img>
         </div>
-      );
+        <div>
+          <h1 className="lg:pt-56 md:pt-48 pt-12 pl-12 text-white font-poppins font-bold lg:text-5xl md:text-3xl text-3xl">
+            {displaySong.name}
+          </h1>
+          <p className="text-xl italic pb-3 font-poppins pl-10 pt-3">
+            {displaySong.artists.join(", ")}
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1>test 2</h1>
+      </div>
+    </>
+  );
 }
