c9f7ccd3ce7de30dcea0de71665c3f7284973844
bigNasY <bobby.j.smith@icloud.com>
2023-06-04 14:34:44 -0700

    name change
    sss

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,59 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Header() {
-    return (
-
-
-      <div className="navbar fixed bg-black font-poppins text-accent text-2xl z-50">
-
-
+  return (
+    <div className="navbar fixed bg-black font-poppins text-accent text-2xl z-50">
       <div className="dropdown">
         <label tabIndex={0} className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
         </label>
-        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-          
-          <li><Link href="/search">Search/Discover</Link></li>
-          <li><Link href="/dashboard">Dashboard</Link></li>
-          <li><Link href="/about">About</Link></li>
+        <ul
+          tabIndex={0}
+          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <Link href="/search">Search/Discover</Link>
+          </li>
+          <li>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
         </ul>
       </div>
 
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-4xl" href="/">SpotiPal</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><Link href="/search">Search/Discover</Link></li>
-            <li><Link href="/dashboard">Dashboard</Link></li>
-            <li><Link href="/about">About</Link></li>
-          </ul>
-        </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-4xl" href="/">
+          MelodyMate
+        </a>
       </div>
-  )
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/search">Search/Discover</Link>
+          </li>
+          <li>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
